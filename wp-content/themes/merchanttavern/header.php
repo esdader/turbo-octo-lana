024db6d10453241c0cd93c8bfdb2cc0b2406da05
Tristan Ader <tristanader@gmail.com>
2014-01-22 14:29:18 -0600

    sketched header html

--- wp-content/themes/merchanttavern/header.php
+++ wp-content/themes/merchanttavern/header.php
@@ -3,14 +3,39 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
 
-        <script src="<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
-        <?php wp_head(); ?>
-    </head>
-    <body>
+    <script src="<?php bloginfo('template_url'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+    <?php wp_head(); ?>
+</head>
+<body>
+<header class="main-header">
+    <div class="cta-btn orange cta-contact">
+        <a href="mailto:brixinv@gmail.com">Contact</a>
+    </div>
+    <div class="cta-btn green cta-latests-drinks">
+        <a href="http://www.themerchanttavern.com/menu/Merchant_latestdrinks.pdf">Latest Drinks</a>
+    </div>
+    <div class="cta-btn orange latest-menu">
+        <a href="http://www.themerchanttavern.com/menu/Merchant_latestmenu.pdf">Latest Menu</a>
+    </div>
+    <div class="cta-msg">
+        <p>Click to download the latest PDF</p>
+    </div>
+    <img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="The Merchant Tavern" class="main-logo">
+    <div class="social-contant">
+        <ul class="l-horizontal">
+            <li class="social-icon"></li>
+            <li><a href="https://www.facebook.com/merchanttavern" class="facebook ir">Facebook</a></li>
+            <li><a href="https://twitter.com/merchanttavern" class="twitter ir">Twitter</a></li>
+            <li><a href="http://instagram.com/merchanttavern" class="instagram ir">Instagram<a></li>
+            <li class="social-arrows"></li>
+            <li><a href="tel:3308659510" class="phone ir">330.865.9510</li>
+        </ul>
+    </div>
+</header>
