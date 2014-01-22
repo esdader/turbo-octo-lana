48ae49d39de7e6093356a1d70290f3faf6e68ecd
Tristan Ader <tristanader@gmail.com>
2014-01-22 14:45:18 -0600

    sketched out footer html

--- wp-content/themes/merchanttavern/footer.php
+++ wp-content/themes/merchanttavern/footer.php
@@ -1,15 +1,29 @@
-        </div> <?php // end container ?>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+<footer class="main-footer">
+    <p class="footer-contact">
+        Copyright <?php echo date('Y'); ?> / The Merchant Tavern LLC / Akron, OH &bull; Designed by <a href="http://www.ohioboy.com">Ohioboy.com</a> &amp; <a href="https://twitter.com/esdader">The Night Owls Union</a>
+    </p>
+    <div class="mini-social-media">
+        <ul class="l-horizontal">
+            <li>Follow Us:</li>
+            <li><a href="https://www.facebook.com/merchanttavern">Facebook</a></li>
+            <li><a href="https://twitter.com/merchanttavern">Twitter</a></li>
+            <li><a href="http://instagram.com/merchanttavern">Instagram</a></li>
+        </ul>
+    </div>
+</footer>    
 
-        <script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
-        <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+</div> <?php // end container ?>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
 
-        <script>
-            // var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            // g.src='//www.google-analytics.com/ga.js';
-            // s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
-    </body>
+<script src="<?php bloginfo('template_url'); ?>/js/plugins.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+
+<script>
+    // var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    // g.src='//www.google-analytics.com/ga.js';
+    // s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+</body>
 </html>
